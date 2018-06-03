f77c602e832e6b5c0ab4328c3e39662f60804a9c
hyunbumy <hyunbumy@usc.edu>
2018-06-03 16:49:18 -0700

    Change db settings

--- rkweb/rkweb/settings.py
+++ rkweb/rkweb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'manager',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'rkweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hyunbumy_remote_key',
+        'USER': 'hyunbumy_rkey_user',
+        'PASSWORD': '303RemotekeyUser!',
+        'HOST': '303.itpwebdev.com',
+        'PORT': '3306',
     }
 }
 
