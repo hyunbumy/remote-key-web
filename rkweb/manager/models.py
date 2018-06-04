88fe2515d049bf7f11aee529112f81ad18df6790
hyunbumy <hyunbumy@usc.edu>
2018-06-03 17:52:15 -0700

    Add Lock model

--- rkweb/manager/models.py
+++ rkweb/manager/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Lock(models.Model):
+    class Meta:
+        db_table = "locks"
+
+    name = models.CharField(max_length=100)
+    ip_address = models.GenericIPAddressField(unique=True)
+    created_by = models.ForeignKey(User, related_name='created_by', on_delete=models.CASCADE)
+    allowed_users = models.ManyToManyField(User, related_name='allowed_users', through='LockPermissions')
+
+class LockPermissions(models.Model):
+    class Meta:
+        db_table = "lock_permissions"
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lock = models.ForeignKey(Lock, on_delete=models.CASCADE)
