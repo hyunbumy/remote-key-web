290eab72347944187894934c1705c3d8fe9e300f
hyunbumy <hyunbumy@usc.edu>
2018-06-13 23:08:25 -0700

    Add req and frontend components

---
+++ rkweb/frontend/src/components/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Test extends React.Component {
+    constructor(props) {
+      super(props)
+  
+      this.state = {
+        username: 'brad'
+      }
+    }
+    render() {
+      return (
+        <div>
+          Hello {this.state.username}
+        </div>
+      )
+    }
+  }
+  
+  ReactDOM.render(<Test />, document.getElementById('app'));
