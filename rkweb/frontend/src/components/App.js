ea476fffe8e7dbd0c99900ad32ada9eaad11c4e8
hyunbumy <hyunbumy@usc.edu>
2018-06-13 23:10:06 -0700

    Indentation for App.js

--- rkweb/frontend/src/components/App.js
+++ rkweb/frontend/src/components/App.js
@@ -2,20 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Test extends React.Component {
-    constructor(props) {
-      super(props)
-  
-      this.state = {
-        username: 'brad'
-      }
-    }
-    render() {
-      return (
-        <div>
-          Hello {this.state.username}
-        </div>
-      )
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: 'brad'
     }
   }
+  render() {
+    return (
+      <div>
+        Hello {this.state.username}
+      </div>
+    )
+  }
+}
   
-  ReactDOM.render(<Test />, document.getElementById('app'));
+ ReactDOM.render(<Test />, document.getElementById('app'));
