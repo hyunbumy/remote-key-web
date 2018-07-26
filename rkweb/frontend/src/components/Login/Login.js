df02d9af0d8983616acdabee9066578b0856752a
hyunbumy <hyunbumy@usc.edu>
2018-07-25 20:08:40 -0700

    Refactor login states

--- rkweb/frontend/src/components/Login/Login.js
+++ rkweb/frontend/src/components/Login/Login.js
@@ -6,7 +6,12 @@ class Login extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { username: '', password: '', loggedIn: false, responseReturned: false };
+    this.state = {
+      username: '',
+      password: '',
+      loggedIn: false,
+      responseReturned: false,
+    };
   }
 
   componentDidMount() {
