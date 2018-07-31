ac846e494f7b15d685e5ae836e51c88e75f6210e
hyunbumy <hyunbumy@usc.edu>
2018-07-30 21:37:00 -0700

    Add access code for locks

--- rkweb/backend/models.py
+++ rkweb/backend/models.py
@@ -6,9 +6,11 @@ class Lock(models.Model):
         db_table = "locks"
 
     name = models.CharField(max_length=100)
+    access_code = models.CharField(max_length=128)
     ip_address = models.GenericIPAddressField(unique=True)
     created_by = models.ForeignKey(User, related_name='created_by', on_delete=models.CASCADE)
     allowed_users = models.ManyToManyField(User, related_name='allowed_users', through='LockPermissions')
+
 
 class LockPermissions(models.Model):
     class Meta:
