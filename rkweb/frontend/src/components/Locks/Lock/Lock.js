bf262c828b3c1866d9613874a49346552222e4af
hyunbumy <hyunbumy@usc.edu>
2018-08-12 10:08:29 -0700

    Button click opens the lock

--- rkweb/frontend/src/components/Locks/Lock/Lock.js
+++ rkweb/frontend/src/components/Locks/Lock/Lock.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+
+import Locks from '../Locks';
 
 const lock = (props) => {
+  const openLock = (ipAddress) => {
+    axios.post(`http://${ipAddress}:8000/locker/`, {
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="container-fluid lock-container border border-primary m-2 p-2" role="presentation">
       <div className="row justify-content-center">
@@ -20,11 +35,17 @@ const lock = (props) => {
           </div>
         </div>
         <div className="col-2">
-          <button onClick={() => alert(props.ipAddr)} className="btn btn-primary">Button</button>
+          <button onClick={() => openLock(props.ipAddr)} className="btn btn-primary">Button</button>
         </div>
       </div>
     </div>
   );
+};
+
+lock.propTypes = {
+  lockName: PropTypes.string.isRequired,
+  ipAddr: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
 };
 
 export default lock;
